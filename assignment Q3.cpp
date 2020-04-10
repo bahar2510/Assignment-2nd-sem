59833bab4863202aba7d2403d5b45ea9ce1d3833
bahar2510 <62321682+bahar2510@users.noreply.github.com>
2020-04-10 13:49:29 +0530

    Add files via upload

---
+++ assignment Q3.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void occur(char *y, int *a);
+int main()
+{
+	char str2[100];
+	char re;
+	int n,i;
+	printf("Enter the string:-");
+	gets(str2);
+	printf("\n enter the character which is repeated:-");
+	scanf("%s",&re);
+	occur(re,n);
+	return 0;
+	
+}
+void occur(char *y,int *a)
+{
+	char str
+}
